91099666aaf4091053e8c72848838d3280c79384
deckvrd <deckvrd@gmail.com>
2020-10-30 19:28:36 -0400

    docs: add create module
    Create two functions. One creates a csv file and the other creates a zip
    archive.

---
+++ libs/createfile.py
@@ -0,0 +1,22 @@
+import time
+import os
+import zipfile
+
+def createFile():
+    now = str(time.time())
+    f = open("docs/demofile" + now + ".csv", "w")
+    f.write("Woops! I have deleted the content!")
+    f.close()
+
+def createZipFile(mylist):
+    now = str(time.time())
+    zf = zipfile.ZipFile("zipFile" + now + ".zip", mode='w')
+    try:
+        os.chdir('docs/')
+        for x in mylist:
+            zf.write(x)
+    finally:
+        print('closing...')
+        zf.close()
+
+
